5a8c46324c3e6a0db3a0409cd852998397f2e4ae
FolkLemur <168951934+FolkLemur@users.noreply.github.com>
2026-06-14 22:04:57 +0000

    last workoutsession is global not per user fix

--- lib/getLastWorkoutSession.ts
+++ lib/getLastWorkoutSession.ts
@@ -1,9 +1,16 @@
 import { supabase } from "./supabase"
 
 export async function getLastWorkoutSession() {
+  const {
+    data: { user }
+  } = await supabase.auth.getUser()
+
+  if (!user) return null
+
   const { data, error } = await supabase
     .from("workout_sessions")
     .select("*")
+    .eq("user_id", user.id)
     .order("created_at", { ascending: false })
     .limit(1)
     .single()
